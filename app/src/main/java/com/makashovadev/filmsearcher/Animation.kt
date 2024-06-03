d613d5ed235cafb68931f05418b99ac723bae19c
mmb <mmb@gitlab.server.com>
2024-06-03 13:32:09 +0300

    Module 22. 1. Several posters were added to the screen using GridLayout and animated using LayoutAnimation during appearances. 2. Poster pressing animation was made using objectAnimator and StateListAnimator. 3. Additionally, the following approaches to creating animation were applied: creating an ObjectAnimator from code and ViewPropertyAnimation.

---
+++ app/src/main/java/com/makashovadev/filmsearcher/Animation.kt
@@ -0,0 +1,57 @@
+package com.makashovadev.filmsearcher
+
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
+import android.animation.LayoutTransition
+import android.animation.ObjectAnimator
+import android.view.View
+import android.view.animation.BounceInterpolator
+import android.view.animation.DecelerateInterpolator
+import android.widget.ImageView
+
+
+// анимация с использованием ObjectAnimator
+fun objectAnimatorTranslationAnim(view: ImageView) {
+    val animatorSet = AnimatorSet()
+    val animDown = ObjectAnimator.ofFloat(view, View.TRANSLATION_Y, 0F, 100F)
+    animDown.duration = 1000
+    animDown.interpolator = BounceInterpolator()
+    val animUp = ObjectAnimator.ofFloat(view, View.TRANSLATION_Y, 100F, 0F)
+    animUp.duration = 1000
+    animUp.interpolator = BounceInterpolator()
+    animatorSet.play(animUp).after(animDown).after(500)
+    animatorSet.start()
+}
+
+// анимация с использованием ObjectAnimator
+fun objectAnimatorScaleAnim(view: ImageView) {
+    val animatorSet = AnimatorSet()
+    val animScaleXStart = ObjectAnimator.ofFloat(view, View.SCALE_X, 1F, 2F)
+    animScaleXStart.duration = 1000
+    animScaleXStart.interpolator = BounceInterpolator()
+    val animScaleYStart = ObjectAnimator.ofFloat(view, View.ROTATION, 0F, 90F)
+    animScaleYStart.duration = 1000
+    animScaleYStart.interpolator = BounceInterpolator()
+    val animScaleXEnd = ObjectAnimator.ofFloat(view, View.SCALE_X, 2F, 1F)
+    animScaleXEnd.duration = 1000
+    animScaleXEnd.interpolator = BounceInterpolator()
+    val animScaleYEnd = ObjectAnimator.ofFloat(view, View.ROTATION, 90F, 0F)
+    animScaleYEnd.duration = 1000
+    animScaleYEnd.interpolator = BounceInterpolator()
+    animatorSet.playTogether(animScaleXStart, animScaleYStart)
+    animatorSet.play(animScaleXEnd).after(animScaleXStart).after(500)
+    animatorSet.play(animScaleYEnd).after(animScaleXStart).after(500)
+    animatorSet.start()
+}
+
+
+//анимация с использованием  ViewPropertyAnimation
+// обработка события нажатия на постер: появляется/исчезает
+fun ViewPropertyAnimation(view: ImageView) {
+    val view_alpha = if (view.alpha == 0f) 1f else 0f
+    view.animate()
+        .setDuration(300)
+        .setInterpolator(DecelerateInterpolator())
+        .alpha(view_alpha)
+        .start()
+}
--- app/src/main/java/com/makashovadev/filmsearcher/MainActivity.kt
+++ app/src/main/java/com/makashovadev/filmsearcher/MainActivity.kt
@@ -1,17 +1,29 @@
 package com.makashovadev.filmsearcher
 
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
+import android.animation.LayoutTransition
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.animation.BounceInterpolator
+import android.view.animation.DecelerateInterpolator
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.makashovadev.filmsearcher.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var container: LinearLayout
+    private lateinit var imageView1: ImageView
+    private lateinit var imageView2: ImageView
+    private lateinit var imageView3: ImageView
+    private lateinit var imageView4: ImageView
+    private lateinit var imageView5: ImageView
 
     // верхнее меню
     private lateinit var topAppBar: MaterialToolbar
@@ -25,13 +37,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
         // инициализация всех компонентов
         init()
+        setUpperPosterAnimator()
     }
 
     // инициализация всех компонентов
     fun init() {
         initTopAppBar()
         initBottomNavigation()
+        container = binding.container
+        imageView1 = binding.imageView1
+        imageView2 = binding.imageView2
+        imageView3 = binding.imageView3
+        imageView4 = binding.imageView4
+        imageView5 = binding.imageView5
     }
+
 
     // инициализация верхнего меню
     fun initTopAppBar() {
@@ -71,6 +91,25 @@ class MainActivity : AppCompatActivity() {
 
                 else -> false
             }
+        }
+    }
+
+    // анисация нажатия на один из верхних постеров
+    fun setUpperPosterAnimator() {
+        imageView1.setOnClickListener {
+            objectAnimatorTranslationAnim(imageView1)
+        }
+        imageView2.setOnClickListener {
+            objectAnimatorScaleAnim(imageView2)
+        }
+        imageView3.setOnClickListener {
+            ViewPropertyAnimation(imageView3)
+        }
+        imageView4.setOnClickListener {
+            ViewPropertyAnimation(imageView4)
+        }
+        imageView5.setOnClickListener {
+            ViewPropertyAnimation(imageView5)
         }
     }
 
