0830c99d466b24cfbbc9e46a6ffd2d18daa424e7
mmb <mmb@gitlab.server.com>
2024-06-11 13:30:37 +0300

    Module 24. 1.Создан RecyclerView для главной страницы с фильмами (пока у нас нет данных по фильмам из сети, будем использовать mock-данные, то есть вручную создадим нашу БД с фильмами (7 - 10 штук)). 2. Сделана заготовку под экран деталей фильма на основе CoordinatorLayout, в которой присутствует Collapsing Toolbar и «привязанные к нему» кнопки В избранное, Посмотреть позже и Поделиться. 3. Дополнительное задание ✱:   Добавлен DiffUtils для нашего RV, на экране деталей добавлен Snackbar на нажатие кнопок В избранное и Посмотреть позже .

---
+++ app/src/main/java/com/makashovadev/filmsearcher/data/dto/Film.kt
@@ -0,0 +1,38 @@
+package com.makashovadev.filmsearcher.data.dto
+
+import android.os.Parcel
+import android.os.Parcelable
+
+//@Parcelize
+data class Film(
+    val title: String,
+    val poster: Int,
+    val description: String
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString().toString(),
+        parcel.readInt(),
+        parcel.readString().toString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(title)
+        parcel.writeInt(poster)
+        parcel.writeString(description)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Film> {
+        override fun createFromParcel(parcel: Parcel): Film {
+            return Film(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Film?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
