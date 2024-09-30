55a30359e19bf7d092ac6d686b3d595bf1c6089d
mmb <mmb@gitlab.server.com>
2024-09-30 20:40:23 +0300

    Сделана загрузку фильмов с помощью Retrofit в RecyclerView на главном экране приложения.
    ✱ Реализована пагинация. (Пагинация в этом случае значит, что когда пользователь доходит до конца загруженной страницы, должна быть загружена следующая страница. Желательно загрузка следующей страницы должна начаться незадолго до конца предыдущей, чтобы создать эффект «бесшовности».)

---
+++ app/src/main/java/com/makashovadev/filmsearcher/data/Entity/API.kt
@@ -0,0 +1,6 @@
+package com.makashovadev.filmsearcher.data.Entity
+
+object API {
+    const val KEY = "a2074ea6bad614ba0402b61b7392cf02"
+    //const val KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMjA3NGVhNmJhZDYxNGJhMDQwMmI2MWI3MzkyY2YwMiIsIm5iZiI6MTcyNzQzNDIxNy4wMjg3NCwic3ViIjoiNjZjNmZjNGUzOWZiMTE5ZDE2ZDdlZGExIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.6C5bMaxsP27dBQjXJeHH6yAEyqPyN_LyAQXeCB09JuE"
+}
